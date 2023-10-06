870a33da1a64a581cb57284725370c9d2fb5f6fc
Sam Mathew <sammathew@Sams-MacBook-Air.local>
2023-10-06 15:55:05 -0400

    new

---
+++ Assignment4-SamMathewUITests/Assignment4_SamMathewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Assignment4_SamMathewUITestsLaunchTests.swift
+//  Assignment4-SamMathewUITests
+//
+//  Created by Sam Mathew on 2023-09-30.
+//
+
+import XCTest
+
+final class Assignment4_SamMathewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
